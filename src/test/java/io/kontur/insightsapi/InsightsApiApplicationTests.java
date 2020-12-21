7809c7dc157f2f6873ecdcb14c340c31e9de79e0
Kira Stepina <kstsepina@kontur.io>
2020-12-21 12:47:26 +0300

    4425: Fix test config, add path to json

--- src/test/java/io/kontur/insightsapi/InsightsApiApplicationTests.java
+++ src/test/java/io/kontur/insightsapi/InsightsApiApplicationTests.java
@@ -3,7 +3,7 @@ package io.kontur.insightsapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class InsightsApiApplicationTests {
 
     @Test
