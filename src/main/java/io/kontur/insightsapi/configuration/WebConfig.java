050870804c4893ef615a4a82486289cbaaeca6a6
Kira Stepina <kstsepina@kontur.io>
2021-09-07 15:55:23 +0300

    #6969 Add date to log, turn on httptrace

--- src/main/java/io/kontur/insightsapi/configuration/WebConfig.java
+++ src/main/java/io/kontur/insightsapi/configuration/WebConfig.java
@@ -1,5 +1,7 @@
 package io.kontur.insightsapi.configuration;
 
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,6 +14,11 @@ import java.util.Collections;
 
 @Configuration
 public class WebConfig {
+
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
 
     @Bean
     public Docket productApi() {
