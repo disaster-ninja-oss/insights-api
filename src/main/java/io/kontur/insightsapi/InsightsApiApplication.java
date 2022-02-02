446b7e1bff206073c4c6a56c6f593da6b1555e17
Kira Stepina <kstsepina@kontur.io>
2022-02-02 12:51:00 +0300

    #8756 Add retry when all results are empty

--- src/main/java/io/kontur/insightsapi/InsightsApiApplication.java
+++ src/main/java/io/kontur/insightsapi/InsightsApiApplication.java
@@ -2,12 +2,14 @@ package io.kontur.insightsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableAsync
+@EnableRetry
 public class InsightsApiApplication {
 
     public static void main(String[] args) {
