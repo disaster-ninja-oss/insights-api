4fe45d424a2cbfc0640d122ecee986d0a0069419
Kira Stepina <kstsepina@kontur.io>
2022-02-15 11:28:47 +0300

    #8789 For correlations calculation hide layers witch does not have any data in geometry

--- src/main/java/io/kontur/insightsapi/repository/StatisticRepository.java
+++ src/main/java/io/kontur/insightsapi/repository/StatisticRepository.java
@@ -1,5 +1,6 @@
 package io.kontur.insightsapi.repository;
 
+import com.google.common.collect.Lists;
 import io.kontur.insightsapi.dto.NumeratorsDenominatorsDto;
 import io.kontur.insightsapi.dto.PolygonStatisticRequest;
 import io.kontur.insightsapi.mapper.*;
@@ -17,7 +18,11 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -431,6 +436,64 @@ public class StatisticRepository {
             logger.error(error);
             throw new IllegalArgumentException(error, e);
         }
+    }
+
+    @Transactional(readOnly = true)
+    public Map<String, Boolean> getNumeratorsForNotEmptyLayersBatch(List<NumeratorsDenominatorsDto> dtoList, String polygon){
+        var paramSource = new MapSqlParameterSource();
+        paramSource.addValue("polygon", polygon);
+        var distinctFieldsRequests = dtoList.stream()
+                .flatMap(dto -> Stream.of(dto.getXNumerator(), dto.getYNumerator()))
+                .distinct()
+                .collect(Collectors.toList());
+        List<String> requests = Lists.newArrayList();
+        for (String field : distinctFieldsRequests) {
+            requests.add("max(" + field + ")!=min(" + field + ") as result" + field);
+        }
+        var query = String.format("""
+                with validated_input as (
+                    select calculate_validated_input(:polygon) geom
+                ),
+                     stat_area as (
+                                 select %s
+                                 from (
+                                          select ST_Subdivide(v.geom, 30) geom
+                                          from validated_input v
+                                      ) p
+                                          cross join
+                                      lateral (
+                                          select h3, %s
+                                          from stat_h3 sh
+                                          where ST_Intersects(sh.geom, p.geom)
+                                          ) h
+                     )
+                select * from stat_area
+                """.trim(), StringUtils.join(requests, ","), StringUtils.join(distinctFieldsRequests, ","));
+        //it is important to disable jit in same stream with main request
+        jitDisable();
+        Map<String, Boolean> result = new HashMap<>();
+        try {
+            namedParameterJdbcTemplate.query(query, paramSource, (rs -> {
+                result.putAll(createResultMap(distinctFieldsRequests, rs));
+            }));
+        } catch (Exception e) {
+            String error = String.format("Sql exception for geometry %s. Exception: %s", polygon, e.getMessage());
+            logger.error(error);
+            throw new IllegalArgumentException(error, e);
+        }
+        return result;
+    }
+
+    private Map<String, Boolean> createResultMap(List<String> numerators, ResultSet rs) {
+        Map<String, Boolean> result = new HashMap<>();
+        numerators.forEach(numerator -> {
+            try {
+                result.put(numerator, rs.getBoolean("result" + numerator));
+            } catch (SQLException e) {
+                logger.error("Can't get boolean value from result set", e);
+            }
+        });
+        return result;
     }
 
     private String createCorrelationQueryString(String xNum, String xDen, String yNum, String yDen) {
