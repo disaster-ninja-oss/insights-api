ee8a0926f4cb49b2158daf14e68c8945940fc316
Kira Stepina <kstsepina@kontur.io>
2022-04-22 17:11:47 +0300

    #9864 Add getTile endpoint

---
+++ src/main/java/io/kontur/insightsapi/controller/TileController.java
@@ -0,0 +1,34 @@
+package io.kontur.insightsapi.controller;
+
+import io.kontur.insightsapi.repository.TileRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Tiles", description = "Tiles API")
+@RestController
+@RequestMapping("/tiles")
+@RequiredArgsConstructor
+public class TileController {
+
+    private final TileRepository tileRepository;
+
+    @Operation(summary = "Get tile using z, x, y.",
+            tags = {"Tiles"},
+            description = "Get tile using z, x, y.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successful operation",
+                            content = @Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "400", description = "Bad Request"),
+                    @ApiResponse(responseCode = "500", description = "Internal error")})
+    @GetMapping("/{z}/{x}/{y}")
+    public byte[] getTile(@PathVariable Integer z, @PathVariable Integer x, @PathVariable Integer y){
+        return tileRepository.getTile(z, x, y);
+    }
+}
