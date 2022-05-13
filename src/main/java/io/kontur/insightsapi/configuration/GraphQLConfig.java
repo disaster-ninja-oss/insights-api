de61d89411eecd3319007706aa1de6084fdd5bc7
Kira Stepina <kstsepina@kontur.io>
2022-05-14 00:02:58 +0300

    #10288 Add DefaultParserOptions

--- src/main/java/io/kontur/insightsapi/configuration/GraphQLConfig.java
+++ src/main/java/io/kontur/insightsapi/configuration/GraphQLConfig.java
@@ -1,15 +1,28 @@
 package io.kontur.insightsapi.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import graphql.parser.ParserOptions;
 import graphql.scalars.ExtendedScalars;
 import graphql.schema.GraphQLScalarType;
 import io.kontur.insightsapi.model.JacksonJsonCoercing;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+
 @Configuration
 public class GraphQLConfig {
+
+    @Value("${graphql-request.parser-options.max-tokens}")
+    Integer maxTokens;
+
+    @PostConstruct
+    public void init() {
+        ParserOptions.setDefaultParserOptions(ParserOptions.newParserOptions()
+                .maxTokens(maxTokens).build());
+    }
 
     @Bean
     public GraphQLScalarType longType() {
