f0a7e3367958fad92489f92b97fbef00151ec35d
isaricicek <isaricicek@kontur.io>
2022-05-20 12:27:48 +0300

    9598 added commnets on changes

--- src/main/resources/sql.queries/advanced_analytics_union.sql
+++ src/main/resources/sql.queries/advanced_analytics_union.sql
@@ -1,4 +1,5 @@
 select avg(sum) filter (where r = 8) as sum_value, case
+        --if value is null, no need to calculate quality???
           when avg(sum) filter (where r = 8) is null then null
           when (nullif(max(sum),0) / nullif(min(sum), 0)) > 0
           then log10(nullif(max(sum), 0) / nullif(min(sum),0))
@@ -7,6 +8,7 @@ end
 as sum_quality,
     avg(min) filter (where r = 8) as min_value,
        case
+        --if value is null, no need to calculate quality???
           when avg(min) filter (where r = 8) is null then null
           when (nullif(max(min),0) / nullif(min(min), 0)) > 0
           then log10(nullif(max(min), 0) / nullif(min(min),0))
@@ -15,6 +17,7 @@ end
 as min_quality,
     avg(max) filter (where r = 8) as max_value,
        case
+          --if value is null, no need to calculate quality???
           when avg(max) filter (where r = 8) is null then null
           when (nullif(max(max),0) / nullif(min(max), 0)) > 0
           then log10(nullif(max(max), 0) / nullif(min(max),0))
@@ -23,6 +26,7 @@ end
 as max_quality,
     avg(mean) filter (where r = 8) as mean_value,
        case
+          --if value is null, no need to calculate quality???
           when avg(mean) filter (where r = 8) is null then null
           when (nullif(max(mean),0) / nullif(min(mean), 0)) > 0
           then log10(nullif(max(mean), 0) / nullif(min(mean),0))
@@ -31,6 +35,7 @@ end
 as mean_quality,
     avg(stddev) filter (where r = 8) as stddev_value,
        case
+          --if value is null, no need to calculate quality???
           when avg(stddev) filter (where r = 8) is null then null
           when (nullif(max(stddev),0) / nullif(min(stddev), 0)) > 0
           then log10(nullif(max(stddev), 0) / nullif(min(stddev),0))
@@ -39,6 +44,7 @@ end
 as stddev_quality,
     avg(median) filter (where r = 8) as median_value,
        case
+          --if value is null, no need to calculate quality???
           when avg(median) filter (where r = 8) is null then null
           when (nullif(max(median),0) / nullif(min(median), 0)) > 0
           then log10(nullif(max(median), 0) / nullif(min(median),0))
@@ -48,15 +54,21 @@ as median_quality
 from (
         select r,
         	case
+    --if sum = 0 probably all values 0 and need to set null to calculate quality
 				when sum(m) = 0 then null
 				else sum(m) end as sum,
+    --filter min != 0 values to be able to calculate quality
         	min(m) filter (where m!=0),
+    --filter max != 0 values to be able to calculate quality
         	max(m) filter (where m!=0),
 			case
+    --if avg = 0 probably all values 0 and need to set null to calculate quality
 			when avg(m) = 0 then null
 				else avg(m) end as mean,
+    --stddev can be 0 when data evenly distributed https://www.quora.com/What-is-meant-by-standard-deviation-is-zero set null for those values to calculate quality
         	case when stddev(m) = 0 then null
 			else stddev(m) end as stddev,
+    --check 0's to calculate quality
 		  case when percentile_cont(0.5) within group (order by m) = 0 then null
 			else percentile_cont(0.5) within group (order by m) end as median
     from (select (%s / nullif(%s,0)) as m, resolution as r from stat_area) z
