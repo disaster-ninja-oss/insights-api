1376fe68df9d375bbe9fa553485cca33036b6260
Ilya Semichastnov <isemichastnov@kontur.io>
2022-05-22 12:25:33 +0300

    #8383 use actual k8s namespaces/gitlab bot for CI/CD

--- external/helm/insights-api/install_upgrade.sh
+++ external/helm/insights-api/install_upgrade.sh
@@ -1,18 +1,24 @@
 CHART_PATH='./external/helm/insights-api/'
 KUBECFG_FILE="./external/kubeconfig.yaml"
-STAGE=$1
-RELEASE=insights-api-$STAGE
+#STAGE=$1 #TODO test it's passed from CI job
+RELEASE=$STAGE-insights-api
+USER=$STAGE-insights-api@kontur.io
 VALUES="$CHART_PATH/values/values-$STAGE.yaml"
 KUBECTL_OPTS="--kubeconfig $KUBECFG_FILE --context $STAGE"
 HELM_OPTS="--kubeconfig $KUBECFG_FILE --kube-context $STAGE"
 
-#Set secrets from Gitlab environment variables #TODO use gitlab bot
+#Set secrets from Gitlab environment variables to bot user
+echo "kubectl $KUBECTL_OPTS config set clusters.$STAGE.certificate-authority-data (secret data)"
 kubectl $KUBECTL_OPTS config set clusters.$STAGE.certificate-authority-data $CERT_AUTH
-kubectl $KUBECTL_OPTS config set users.isemichastnov@kontur.io.client-key-data $CLIENT_KEY
-kubectl $KUBECTL_OPTS config set users.isemichastnov@kontur.io.client-certificate-data $CLIENT_CERT
+echo "kubectl $KUBECTL_OPTS config set users.$USER.client-key-data (secret data)"
+kubectl $KUBECTL_OPTS config set users.$USER.client-key-data $CLIENT_KEY
+echo "kubectl $KUBECTL_OPTS config set users.$USER.client-certificate-data (secret data)"
+kubectl $KUBECTL_OPTS config set users.$USER.client-certificate-data $CLIENT_CERT
 
 #render templates to save as Gitlab job artifacts
+echo "helm $HELM_OPTS template $RELEASE $CHART_PATH -f $VALUES > manifests.yaml"
 helm $HELM_OPTS template $RELEASE $CHART_PATH -f $VALUES > manifests.yaml
 
-#Check if specified release exists
+#install or upgrade the release
+echo "helm $HELM_OPTS upgrade --install $RELEASE $CHART_PATH -f $VALUES"
 helm $HELM_OPTS upgrade --install $RELEASE $CHART_PATH -f $VALUES
