5011b50e19e65fdd7127e80079712b30a3a6bc17
Kira Stepina <kstsepina@kontur.io>
2022-06-06 20:50:41 +0300

    #6809 Add parameter with the class of requested indicator

--- src/main/java/io/kontur/insightsapi/controller/TileController.java
+++ src/main/java/io/kontur/insightsapi/controller/TileController.java
@@ -6,10 +6,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Tiles", description = "Tiles API")
 @RestController
@@ -28,10 +25,11 @@ public class TileController {
                     @ApiResponse(responseCode = "400", description = "Bad Request"),
                     @ApiResponse(responseCode = "500", description = "Internal error")})
     @GetMapping(value = "/bivariate/v1/{z}/{x}/{y}.mvt", produces = "application/vnd.mapbox-vector-tile")
-    public byte[] getBivariateTileMvt(@PathVariable Integer z, @PathVariable Integer x, @PathVariable Integer y) {
+    public byte[] getBivariateTileMvt(@PathVariable Integer z, @PathVariable Integer x, @PathVariable Integer y,
+                                      @RequestParam(defaultValue = "all") String indicatorsClass) {
         if (z < 0 || z > 8 || x < 0 || x > (Math.pow(2, z) - 1) || y < 0 || y > (Math.pow(2, z) - 1)) {
             return new byte[0];
         }
-        return tileService.getBivariateTileMvt(z, x, y);
+        return tileService.getBivariateTileMvt(z, x, y, indicatorsClass);
     }
 }
