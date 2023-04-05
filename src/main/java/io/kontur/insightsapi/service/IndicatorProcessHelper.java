c0f4232cdcfcb6e28d1de60cffce017df94eaa45
Ivan Shilyaev <112929445+i1shilyaev@users.noreply.github.com>
2023-04-05 12:32:54 +0200

    #15093 Calculate stops and quality asynchronously in the background (#42)
    * #15093 Perform calculations for indicator asynchronously
    
    * #15093 Use custom ThreadPoolExecutor. Refactoring

--- src/main/java/io/kontur/insightsapi/service/IndicatorProcessHelper.java
+++ src/main/java/io/kontur/insightsapi/service/IndicatorProcessHelper.java
@@ -1,38 +1,65 @@
 package io.kontur.insightsapi.service;
 
 import io.kontur.insightsapi.dto.BivariateIndicatorDto;
-import io.kontur.insightsapi.repository.IndicatorRepository;
+import io.kontur.insightsapi.dto.IndicatorState;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PreDestroy;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @AllArgsConstructor
 public class IndicatorProcessHelper {
 
+    private static final Logger logger = LoggerFactory.getLogger(IndicatorProcessHelper.class);
+
     private final IndicatorService indicatorService;
 
     private final AxisService axisService;
 
-    private final IndicatorRepository indicatorRepository;
-
     private static final int UUID_STRING_LENGTH = 36;
+
+    private static final int CORE_POOL_SIZE = 10;
+
+    private static final int MAX_POOL_SIZE = 20;
+
+    private static final int MAX_QUEUE_SIZE = 200;
+
+    private static final ThreadPoolExecutor executor = new ThreadPoolExecutor(CORE_POOL_SIZE, MAX_POOL_SIZE,
+            60, TimeUnit.SECONDS,
+            new LinkedBlockingQueue<>(MAX_QUEUE_SIZE));
 
     public ResponseEntity<String> processIndicator(HttpServletRequest request) {
 
-        ResponseEntity<String> response = indicatorService.uploadIndicatorData(request);
+        final ResponseEntity<String> response = indicatorService.uploadIndicatorData(request);
 
-//        TODO: this part have to be run async in background. Response have to be returned right after the CSV file was uploaded, all calculations in parallel
-        if (response.getStatusCode().is2xxSuccessful() && response.hasBody() && response.getBody().length() >= UUID_STRING_LENGTH) {
-            List<BivariateIndicatorDto> incomingBivariateIndicatorDtoAsList =
-                    List.of(indicatorRepository.getIndicatorByUuid(response.getBody().substring(response.getBody().length() - UUID_STRING_LENGTH)));
-
-            response = axisService.createAxis(incomingBivariateIndicatorDtoAsList);
-        }
+        executor.submit(() -> {
+            if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null
+                    && response.getBody().length() >= UUID_STRING_LENGTH) {
+                String uuid = response.getBody().substring(response.getBody().length() - UUID_STRING_LENGTH);
+                List<BivariateIndicatorDto> incomingBivariateIndicatorDtoAsList =
+                        List.of(indicatorService.getIndicatorByUuid(uuid));
+                logger.info("Start calculations for indicator with uuid {}", uuid);
+                axisService.createAxis(incomingBivariateIndicatorDtoAsList);
+                logger.info("Calculations for indicator with uuid {} have been done successfully", uuid);
+                indicatorService.updateIndicatorState(uuid, IndicatorState.READY);
+            }
+        });
+        logger.info("Current queue size with indicators to process: {}", executor.getQueue().size());
 
         return response;
+    }
+
+    @PreDestroy
+    public void shutdown() {
+        executor.shutdown();
     }
 }
