1ec9bcd2be02499fb6c8e42a5bcc825db72ee28f
kshmidman <kshmidman@gmail.com>
2023-05-05 11:04:14 +0300

    #14032. added h3 index to geometry for bivariate tiles

--- src/main/resources/sql.queries/get_tile_mvt_generate_on_the_fly.sql
+++ src/main/resources/sql.queries/get_tile_mvt_generate_on_the_fly.sql
@@ -4,9 +4,10 @@ with res as (select sg.geom, sg.h3, st.indicator_uuid, st.indicator_value
              where sg.geom && ST_TileEnvelope(:z, :x, :y)
                and sg.zoom = :resolution
                and indicator_uuid IN (%s))
-select ST_AsMVT(q, 'stats', 8192, 'geom') as tile
+select ST_AsMVT(q, 'stats', 8192, 'geom', 'h3ind') as tile
 from (select
                   %s,
-                  ST_AsMVTGeom(geom, ST_TileEnvelope(:z, :x, :y), 8192, 64, true) as geom
+                  ST_AsMVTGeom(geom, ST_TileEnvelope(:z, :x, :y), 8192, 64, true) as geom,
+                  h3index_to_bigint(h3) as h3ind
       from res
-      group by geom) q;
+      group by geom, h3) q;
