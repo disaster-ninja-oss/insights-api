fe957c76c7ca8ea1a242c97500a4cb52a194d7fa
Ivan Shilyaev <112929445+i1shilyaev@users.noreply.github.com>
2023-08-11 10:21:42 +0200

    #14853 Update POST /indicators/upload endpoint description (#62)
    * #14853 Update POST /indicators/upload endpoint description
    
    * #14853 Update description according to comments

--- src/main/java/io/kontur/insightsapi/controller/IndicatorController.java
+++ src/main/java/io/kontur/insightsapi/controller/IndicatorController.java
@@ -25,11 +25,37 @@ public class IndicatorController {
 
     private final IndicatorProcessHelper indicatorProcessHelper;
 
-    @Operation(summary = "Creates or updates data about specific indicator.",
+    @Operation(
+            summary = "Create or update data about specific indicator.",
             tags = {"Indicators"},
-            description = "Uploads data from CSV file into stat_h3 table and calculates insights for indicator.",
+            description = "Upload data representing h3Index - indicatorValue pairs in the form of CSV file " +
+                    "(no header) alongside indicator metadata. After data has been successfully uploaded, " +
+                    "the response with indicator unique identifier (uuid) is returned and calculations for " +
+                    "indicator start in the background." +
+
+                    "<br><br>Currently files can't be uploaded via Swagger due to endpoint " +
+                    "implementation specifics." +
+
+                    "<br><br>Curl general example: curl -w \":::\"%{http_code} --location --request POST " +
+                    "https://apps.kontur.io/insights-api/indicators/upload " +
+                    "--header 'Authorization: <ACCESS_TOKEN> " +
+                    "--form 'parameters=\"{\\\"id\\\": ${layer_id}, \\\"label\\\": ${layer_label}, " +
+                    "\\\"direction\\\": ${layer_direction}, \\\"isBase\\\": ${layer_isbase}, \\\"isPublic\\\": " +
+                    "${layer_ispublic}, \\\"copyrights\\\": ${layer_copyrights}, \\\"description\\\": " +
+                    "${layer_description}, \\\"coverage\\\": ${layer_coverage}, \\\"updateFrequency\\\": " +
+                    "${layer_update_freq}, \\\"unitId\\\": ${layer_unit_id}, \\\"lastUpdated\\\": " +
+                    "${layer_last_updated}}\" " +
+                    "--form 'file=@\"/path/to/file/indicator.csv\"'" +
+
+                    "<br><br>Curl example with parameters: curl -w \":::\"%{http_code} --location --request POST " +
+                    "https://apps.kontur.io/insights-api/indicators/upload --header " +
+                    "'Authorization: Bearer <ACCESS_TOKEN>' --form 'parameters={\"id\": \"area_km2\", \"label\": " +
+                    "\"Area\", \"direction\": [[\"neutral\"], [\"neutral\"]], \"isBase\": true, \"isPublic\": false, " +
+                    "\"copyrights\": [\"Concept of areas © Brahmagupta, René Descartes\"], \"description\": \"\", " +
+                    "\"coverage\": \"World\", \"updateFrequency\": \"static\", \"unitId\": \"km2\", \"lastUpdated\": " +
+                    "\"\"}' --form 'file=@\"data/area_km2.csv\"'",
             responses = {
-                    @ApiResponse(responseCode = "200", description = "Successful operation"),
+                    @ApiResponse(responseCode = "200", description = "Successful upload"),
                     @ApiResponse(responseCode = "400", description = "Bad Request"),
                     @ApiResponse(responseCode = "500", description = "Internal error")})
     @PostMapping(value = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
