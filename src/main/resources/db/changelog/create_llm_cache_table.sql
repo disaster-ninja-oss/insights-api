0376be0ac3b3f1a65ff216ffe7eeef9136713bfa
sleeping <hh.sleeping@gmail.com>
2024-04-30 18:59:45 +0400

    switch llm_cache hash to UUID, add model_name

--- src/main/resources/db/changelog/create_llm_cache_table.sql
+++ src/main/resources/db/changelog/create_llm_cache_table.sql
@@ -3,8 +3,9 @@
 
 create table if not exists llm_cache
 (
-    hash        char(32) primary key, -- md5 hash of request
+    hash        uuid primary key, -- md5 hash of request in UUID format
     request     text,
     response    text,
+    model_name  text,
     created_at  timestamptz default now()::timestamptz
 );
