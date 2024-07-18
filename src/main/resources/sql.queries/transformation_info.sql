13a90a1f44cafa4bc54e6fe9938ab258223252ae
sleeping <hh.sleeping@gmail.com>
2024-07-18 17:25:28 +0400

    19081 - transformation_info.sql - return result by param_id

--- src/main/resources/sql.queries/transformation_info.sql
+++ src/main/resources/sql.queries/transformation_info.sql
@@ -1,3 +1,4 @@
 select jsonb_array_elements(transformations)
 from bivariate_axis_v2
-where numerator_uuid = ?::uuid and denominator_uuid = ?::uuid
+where numerator_uuid = (select internal_id from bivariate_indicators_metadata where param_id = ? and state = 'READY' and is_public limit 1)
+and denominator_uuid = (select internal_id from bivariate_indicators_metadata where param_id = ? and state = 'READY' and is_public limit 1)
