dc5379bb307d70585b6070409fafa212a5b4c676
sleeping <hh.sleeping@gmail.com>
2025-01-06 10:49:42 +0000

    fix duplicated hexes in graphql/functions

--- src/main/resources/sql.queries/function_intersect_v2.sql
+++ src/main/resources/sql.queries/function_intersect_v2.sql
@@ -3,11 +3,13 @@ with validated_input as (
 ),
      boxinput as (select st_envelope(v.geom) as bbox from validated_input as v),
      subdivision as (select st_subdivide(v.geom) geom from validated_input v),
-     res as (select %s
+     hexes as (select distinct sh.h3
              from boxinput bi
                       cross join subdivision sb
                       join stat_h3_geom sh on (sh.geom && bi.bbox and st_intersects(sh.geom, sb.geom))
+             where sh.resolution = 8),
+     res as (select %s from hexes sh
                       %s
-             where sh.resolution = 8)
+        )
 select %s
 from res st;
