4e9311e3ca4e68c56d0ce9f6e549deb5ad911f42
Darafei Praliaskouski <darafei@kontur.io>
2025-03-22 16:24:55 +0300

    Simplify joins in function_intersect_v2

--- src/main/resources/sql.queries/function_intersect_v2.sql
+++ src/main/resources/sql.queries/function_intersect_v2.sql
@@ -1,15 +1,18 @@
-with validated_input as (
-    select (:polygon)::geometry as geom
-),
-     boxinput as (select st_envelope(v.geom) as bbox from validated_input as v),
-     subdivision as (select st_subdivide(v.geom) geom from validated_input v),
+with 
+     validated_input as (select (:polygon)::geometry as geom),
+     boxinput as (select ST_Envelope(v.geom) as bbox from validated_input as v),
+     subdivision as (select ST_Subdivide(v.geom) geom from validated_input v),
      hexes as (select distinct sh.h3
-             from boxinput bi
-                      cross join subdivision sb
-                      join stat_h3_geom sh on (sh.geom && bi.bbox and st_intersects(sh.geom, sb.geom))
-             where sh.resolution = 8),
+             from 
+                stat_h3_geom sh,
+                subdivision sb
+             where 
+                sh.resolution = 8
+                and sh.geom && (select bbox from boxinput)
+                and ST_Intersects(sh.geom, sb.geom)                
+     ),
      res as (select %s from hexes sh
                       %s
-        )
+     )
 select %s
 from res st;
